0722848c4de2cb80854c37fe01091403bb4d9b43
dnatisdale <dant.grnt@gmail.com>
2026-05-14 15:29:14 -0700

    favicon change

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.svg", "icon-192.png", "icon-512.png"],
+      includeAssets: ["favicon.png", "icon-192.png", "icon-512.png"],
       manifest: {
         name: "Our TuckAway",
         short_name: "TuckAway",
